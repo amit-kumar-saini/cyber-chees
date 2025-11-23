4a33cd46c0694a37c3538c35eb93b14489f118c3
amit kumar saini <amitsaini@shoolini.online>
2025-11-23 17:32:12 +0530

    fix: export capacitor config as TypeScript module

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,15 @@
-{
-  "appId": "com.cyber.chess",
-  "appName": "cyber-chess",
-  "webDir": "dist",
-  "plugins": {
-    "AdMob": {
-      "appId": "ca-app-pub-3940256099942544~3347511713",
-      "initializeForTesting": true
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cyber.chess',
+  appName: 'cyber-chess',
+  webDir: 'dist',
+  plugins: {
+    AdMob: {
+      appId: 'ca-app-pub-3940256099942544~3347511713',
+      initializeForTesting: true
     }
   }
-}
+};
+
+export default config;
