0727888ca9a1d7256da2875ea2054c2e8ddc86d5
amit kumar saini <amitsaini@shoolini.online>
2026-04-05 12:26:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "./", // <--- यह सबसे ज़रूरी लाइन है जिसे आपको जोड़ना है
   server: {
     host: "::",
     port: 8080,
